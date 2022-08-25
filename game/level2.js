2684718c6fe87bd9c79b3e324b62b6260592114f
Koei Ural <koei.ural@gmail.com>
2022-08-25 11:21:27 +0200

    Added preliminary L2 implementation

--- game/level2.js
+++ game/level2.js
@@ -6,6 +6,7 @@ class Level2 extends BaseLevel {
         super();
 
         this.ship = player;
+        this.bg = new PerlinStarsBG(engine.cw, engine.ch);
         
     }
 
@@ -18,11 +19,39 @@ class Level2 extends BaseLevel {
     }
 
     update() {
+        let isLevelEnd = false;
 
+        // If music is not playing, start it
+        if (musicSet["L1"].isPlaying() === false) {  // TODO: load the music for Level 2
+            musicSet["L1"].loop();
+        }
+
+        // Update BG
+        this.bg.update();
+
+        // Move the ship with keyboard inputs
+        engine.game.movePlayer();
+
+        // ...
+
+        // Check if the game is over
+        if (this.ship.isDead()) {
+            engine.phase = DEAD;
+            musicSet["L1"].stop(); // TODO: change L1 key with variable of the level
+        } else {
+            // Increment level difficulty
+            //isLevelEnd = this.levelUpdate(frameCount);
+        }
+
+        return isLevelEnd;
     }
 
     show() {
+        // draw the BG
+        this.bg.show();
 
+        // Draw the ship
+        this.ship.show();
     }
 
     resize(xs, ys) {

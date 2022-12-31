c9eef8fd6796a9154eba493b523f474ce507d66c
KoeiUral <koei.ural@gmail.com>
2022-12-31 16:41:40 +0100

    Changed game title

--- game/game.js
+++ game/game.js
@@ -89,7 +89,7 @@ class Game {
         textFont(fontSet["TEXTF"]);
         textAlign(CENTER, CENTER);
         textSize(150 * engine.ch / DEFAULT_H);
-        text("SPACE \nSMUGGLERz", engine.cw / 2, engine.ch / 2 - engine.ch / 10);
+        text("SpaceApes\nCONTINUUM", engine.cw / 2, engine.ch / 2 - engine.ch / 10);
 
         engine.gui.displayContinueMsg("SPACE", "start");
     }
